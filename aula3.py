fb9a563fd0b1502fdf7312e168979da5d9dada7b
cesaragomes <cesar.augusto.gomes@outlook.com>
2024-09-13 10:35:51 -0300

    nao tenho ideia do que estou fazendo

---
+++ aula3.py
@@ -0,0 +1,20 @@
+#Strings são textos dentro de aspas
+
+print("César Augusto") #Aspas duplas
+
+print('César Augusto') #Aspas simples
+
+print("César \"Augusto") #Ordena para não fechar a string. As aspas duplas serão exibidas
+
+print("César \"Augusto\"") #Ordena para não fechar a string. As aspas duplas serão exibidas
+
+print(r"César \"Augusto") #O r vai exibir o caractér de barra
+
+print(r"César \"Augusto\"") #O r vai exibir o caractér de barra
+
+print('"César Augusto"') #Vai exibir as aspas duplas pois a string começa com aspas simples
+
+print("'César Augusto'") #Vai exibir as aspas simples pois a string começa com aspas duplas
+
+
+
